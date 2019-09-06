9705d7db8abfad0be286fb1654b836c724360b3e
KyuTaeKim <kimk851@newschool.edu>
2019-09-06 13:52:04 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("test")
+
+datatypes
+
+
+"string"
+
+21.1 
+
+/* boolean: true or false 
+
+   array: ['string',
+   'another string',
+    22,
+     true,
+     [inside of array]] list of value 
+
+     false
+
+
+     object
+
+     {
+		'key': 'value',
+		'anotherkey': false, 
+		'key3': [1,2,3],
+		'key4 : {
+			'key' : 'value',
+			'object' : {
+				'title' : 'another value'
+	
+			}
+		}
+     }
+
+     [1,2,3,4].length
+
+     [1,2,3,4]
+
+
+     const
+     let 
+     var 
+
+     
+
+
---
+++ script1.js
@@ -0,0 +1,87 @@
+
+
+
+var myVariable = 'this is a string';
+
+var minute = 17;
+
+var className = 'Web Advanced'; 
+
+var myArray = [];
+
+myArray = [2,3,4];  /* You can redefine */
+
+var addition = (myOtherNumber + 4) / myNumber
+
+var myFirstValue = 5;
+var mySecondValue = 5; 
+var myThirdValue = 'sample string';
+
+
+5 > 3 // true
+
+5 > 6 // false
+
+5 >=5 // true
+
+5<=5 // true
+
+5 == 4 // false 
+
+5 ===5 // true 
+
+5 != 5 // false 
+
+5!= 4 // true 
+
+5 !==5 // false 
+
+if (true) {
+
+	// do some thing 
+	//comparison 
+}
+
+if ( 5 > 4) {
+
+	// do some thing 
+	//comparison
+
+	console.log('hello!') 
+}
+
+if ( myFirstValue !== mySecondValue) {
+
+	// do some thing 
+	//comparison
+
+	console.log('hello!') 
+}else{
+	console.log('hello!')
+}
+
+if ( myFirstValue !== mySecondValue) {
+
+	// do some thing 
+	//comparison
+
+	console.log('hello!') 
+}else if{
+	console.log('hello!')
+}
+
+if ( myFirstValue === mySecondValue || myThirdValue === 'sample string')
+{
+	console.log('hello from if!')
+
+}
+
+console.log(modulus)
+
+var modulus = 5 % 2;
+
+console.log(modulus)
+
+
+
+
